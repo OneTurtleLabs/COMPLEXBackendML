5f5343c3ec8d57614f62a9c572c81a93777778a1
Wojtechnology <wswiders@uwaterloo.ca>
2015-04-09 19:18:16 -0400

    Token getting

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # COMPLEXBackendML
 OMG CSSS COMPLEX BACKEND FOR CSSS MMLL
+
+https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=4ng71gqx25e27yig6whdsbkgx3q2my8&redirect_uri=http://rostislav.me&scope=
--- server.py
+++ server.py
@@ -9,7 +9,7 @@ API_VERSION = 'v3'
 # Client ID for application
 CLIENT_ID = '4ng71gqx25e27yig6whdsbkgx3q2my8'
 # Number of seconds between api calls
-SAVE_PERIOD = 60
+SAVE_PERIOD = 2
 # Number of featured streams to get (MAX 100)
 FEAT_LIMIT = 100
 
@@ -33,6 +33,7 @@ def save_featured_streams(headers):
 	response = get_featured_streams(headers)
 	featured = response['featured']
 
+
 	if featured is not None:
 		for feature in featured:
 			stream = feature['stream']
@@ -48,10 +49,12 @@ if __name__ == '__main__':
 
 	# Value that helps compensated for time drift
 	prev_time = time.time()
+	iteration = 0
 
 	# Event loop
 	while True:
 		time.sleep(SAVE_PERIOD - (time.time() - prev_time))
 		prev_time = time.time()
 		save_featured_streams(headers)
-		print time.time()
+		iteration += 1
+		print 'Iteration: ' + str(iteration) + ' on ' + time.ctime()
