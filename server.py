ee919ee0a265ac2aeb24c084e57549b9ae480309
wojtechnology <wswiders@uwaterloo.ca>
2015-04-09 20:30:21 -0400

    MongoDB saving streams

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import time
+import datetime
+
+from pymongo import MongoClient
 
 # The base url for the Twitch API
 BASE_URL = 'https://api.twitch.tv/kraken'
@@ -9,9 +12,13 @@ API_VERSION = 'v3'
 # Client ID for application
 CLIENT_ID = '4ng71gqx25e27yig6whdsbkgx3q2my8'
 # Number of seconds between api calls
-SAVE_PERIOD = 2
+SAVE_PERIOD = 60
 # Number of featured streams to get (MAX 100)
 FEAT_LIMIT = 100
+
+# Shit for MongoDB
+MONGO_PORT = 1337
+MONGO_URL = 'localhost'
 
 # Will return a dictionary of featured streams
 def get_featured_streams(headers):
@@ -28,18 +35,23 @@ def get_featured_streams(headers):
 		return None
 
 # Save stream data
-def save_featured_streams(headers):
+def save_featured_streams(headers, streams):
 
 	response = get_featured_streams(headers)
 	featured = response['featured']
 
-
 	if featured is not None:
 		for feature in featured:
 			stream = feature['stream']
+			stream['datetime'] = datetime.datetime.utcnow()
+			ins_stream = streams.save(stream)
 
 # runs
 if __name__ == '__main__':
+	# Setup MongoDB
+	client = MongoClient(MONGO_URL, MONGO_PORT)
+	db = client.twitchml
+	streams = db.streams
 
 	# Headers for every request
 	headers = {
@@ -55,6 +67,6 @@ if __name__ == '__main__':
 	while True:
 		time.sleep(SAVE_PERIOD - (time.time() - prev_time))
 		prev_time = time.time()
-		save_featured_streams(headers)
+		save_featured_streams(headers, streams)
 		iteration += 1
 		print 'Iteration: ' + str(iteration) + ' on ' + time.ctime()
